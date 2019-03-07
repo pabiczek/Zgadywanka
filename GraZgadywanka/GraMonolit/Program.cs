33817013e6f74e64468529c4db3f5d2a8a8d6c6e
jpabijan <jakub.pabijan@microsoft.wsei.edu.pl>
2019-03-07 14:48:06 +0100

    wersja 0.1

---
+++ GraZgadywanka/GraMonolit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra Za dużo za mało");
+
+
+            Console.Write("Podaj imię!: ");
+            string nazwa = Console.ReadLine();
+            Console.WriteLine($"Witaj, {nazwa}");
+
+            Console.WriteLine("Koniec gry");
+        }
+    }
+}
